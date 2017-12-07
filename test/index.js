f0ea600f823a1c3568d9cfbe250180a3dba2517b
tmarshall <timothyjmarshall@gmail.com>
2017-12-07 12:34:54 -0800

    connect() test

--- test/index.js
+++ test/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import 'jsdom-global/register'; // for enzyme .mount to work
+
+import React, { Component } from 'react';
 import { test } from 'ava';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import Federal from '../dist/federal';
+import Federal, { connect } from '../dist/federal';
 
 Enzyme.configure({
   adapter: new Adapter()
@@ -17,4 +19,24 @@ test('<Federal /> should render children', t => {
   );
 
   t.true(content.contains(<div>hello world</div>));
+});
+
+test('connect() should carry down store attributes', t => {
+  const initialStore = {
+    a: 'x',
+    b: 'y',
+    c: 'z'
+  };
+
+  const ConnectedChild = connect(store => store)(
+    ({ a, b, c }) => (<div id='child'>{c}{b}{a}</div>)
+  );
+
+  const content = mount(
+    <Federal store={initialStore}>
+      <ConnectedChild />
+    </Federal>
+  );
+
+  t.true(content.find('#child').text() === 'zyx');
 });
